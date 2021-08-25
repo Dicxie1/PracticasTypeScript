79726c49b1a05e38ac9e9e67d1dc10f81ae0df1b
Dicxie Madrigal <hidannara@yahoo.es>
2021-08-25 15:19:04 -0600

    Agregar un script script02.ts al directorio 01-variable

---
+++ 01-variable/script03.ts
@@ -0,0 +1,20 @@
+/*Variable in TypeScript */
+
+/* variable declaration : 
+    let variable: <type> = value
+    let variable = value 
+
+  <type>:
+    Number
+    String
+    Boolena
+
+
+*/
+let esMayor: boolean = true;
+let edad : number = 30;
+let nombre: string = "Dicxie Madrigal";
+
+let hermanos: string[] = ["Denwrite", "Denslee", "Glenda"];
+
+let varios : any[] = [true, "Inicio", 30];
