30c390e4eaa60273b38820b50c6593c252226904
Dicxie Madrigal <hidannara@yahoo.es>
2021-08-27 10:34:30 -0600

    Problemas con la fusion resulto 01-variable/script03.ts

--- 01-variable/script03.ts
+++ 01-variable/script03.ts
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-function saludo(nombre:String): String{
-    return `Bienvenido a la programaciòn en TypeScrip ${nombre}`;
-}
-
-console.log(saludar('Dicxie'));
-=======
 /*Variable in TypeScript */
 
 /* variable declaration : 
@@ -25,4 +18,3 @@ let nombre: string = "Dicxie Madrigal";
 let hermanos: string[] = ["Denwrite", "Denslee", "Glenda"];
 
 let varios : any[] = [true, "Inicio", 30];
->>>>>>> 9a1b35f6ae6fe2a55c5d67a3209db291ee995969
