4a26e1b9e9ca8f36148aa1d58b940b927c91cae1
Dicxie Madrigal <hidannara@yahoo.es>
2021-08-27 10:24:48 -0600

    practica de tipos de variables

--- 01-variable/script04.ts
+++ 01-variable/script04.ts
@@ -1,11 +1,14 @@
 //declariacion de cadena de caráctrer
-let variable01: string = "Hola Mundo";
-let variable02: string = 'Otra variable';
-let variable03: string = `en TypeScript puede Utilizar las comillas doble, comilla simple y las especiales `;
+let saluda: string = "Hola Mundo";
+let variable: string = 'Otra variable';
+let mensaje: string = `en TypeScript puede Utilizar las comillas doble, comilla simple y las especiales `;
 
 //declaracion de Number 
-let variable04 :number = 7; // entero
-let variable05 :number = 20.3; // flotante
-let variable06 :number = 40; // flotante
+let numero1 :number = 7; // entero
+let flotante :number = 20.3; // flotante
+let numero2 :number = 40; // flotante
 
-let variable07: number = 30;
+//declaraciones boolenas 
+let apagado: boolean = true;
+let isOld: boolean = false;
+
--- 01-variable/script05.ts
+++ 01-variable/script05.ts
@@ -1,9 +1,13 @@
 interface User{
     email:string;
     id: number;
+    name(params:number):void;
 }
 
 const user: User ={
     email : "dicxiemadrigal@gmail.com",
-    id:  20
+    id:  20,
+    name(para:number):void{
+        console.log("Hola Mundo")
+    }
 }
